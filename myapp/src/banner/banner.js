625cfba74158d1bb1c0eac176945c7f8be3aa25c
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-10-24 10:21:55 -0300

    banner

--- myapp/src/banner/banner.js
+++ myapp/src/banner/banner.js
@@ -1,14 +1,37 @@
 import './banner.css'
 import ControlledCarousel from './fotoBanner'
+import '../cssGlobal/global.css'
+
+//img
+import caminhao from '../img/caminhao.png'
+import card from '../img/card.png'
+import seg from '../img/seguranca.png'
+
 
 export default function Banner(){
 
     return(
-        <div>
-            <div className='banners'>
-                <ControlledCarousel />
+        <div className='fundo'>
+            <div className='flex setenta'>
+                <div className='banners'>
+                    <ControlledCarousel />
+                </div>
+
+                <div className='boxGiant'>
+                    <div className='boxMenor'>
+                        <img src={caminhao}/>
+                        <p>Entregamos para todo o Brasil</p>
+                    </div>
+                    <div className='boxMenor'>
+                        <img src={card}/>
+                        <p>Parcelamos em até 6x no Cartão</p>
+                    </div>
+                    <div className='boxMenor'>
+                        <img src={seg}/>
+                        <p>Site 100% Seguro</p>
+                    </div>
+                </div>
             </div>
-            
    
         </div>
     )
---
+++ myapp/src/produtos/produtos.js
@@ -0,0 +1,14 @@
+import './produtos.css'
+import '../cssGlobal/global.css'
+
+export default function Produtos(){
+
+    return(
+        <div className='fundo'>
+            <div className='produtos'>
+                
+
+            </div>
+        </div>
+    )
+}
